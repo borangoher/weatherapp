186db4555a82dc6ddcd93c4059edd1afc8d2a165
borangoher <goherboran1337@gmail.com>
2022-07-24 16:57:45 +0300

    format with prettier

--- script.js
+++ script.js
@@ -12,33 +12,52 @@ const humVal = document.getElementById("hum");
 const presVal = document.getElementById("pres");
 
 const createURL = function (inputElem, key) {
-    const query = inputElem.value;
-    const URL = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&APPID=" + key;
-    return URL;
-}
+  const query = inputElem.value;
+  const URL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&APPID=" +
+    key;
+  return URL;
+};
 
 const fetchData = async function (URL) {
-    const fetchRes = await fetch(URL, {mode: 'cors'});
-    const fetchJSON = await fetchRes.json();
-    return fetchJSON;
-}
+  const fetchRes = await fetch(URL, { mode: "cors" });
+  const fetchJSON = await fetchRes.json();
+  return fetchJSON;
+};
 
 const toCelcStr = function (num) {
-    let celcVal = num - 273.15;
-    return celcVal.toFixed(2);
-}
+  let celcVal = num - 273.15;
+  return celcVal.toFixed(2);
+};
 
 const displayData = async function (weatherJSON) {
-    const weatherObject = await weatherJSON.then(function(response) {return response});
-    weatherHead.textContent = "Currently viewing data for: " + weatherObject.name;
-    weatherType.innerHTML = weatherObject.weather[0].main + `
-                            <small class="text-muted" id="desc">` + weatherObject.weather[0].description + `</small>`;
-    tempVal.textContent = "Temperature: " + toCelcStr(weatherObject.main.temp) + "°C";
-    tempApp.textContent = "Apparent Temperature: " + toCelcStr(weatherObject.main.feels_like) + "°C";
-    tempMax.textContent = "Maximum Temperature: " + toCelcStr(weatherObject.main.temp_max) + "°C";
-    tempMin.textContent = "Minimum Temperature: " + toCelcStr(weatherObject.main.temp_min) + "°C";
-    humVal.textContent = "Humidity: " + weatherObject.main.humidity.toFixed(2) + "%";
-    presVal.textContent = "Pressure: " + weatherObject.main.pressure.toFixed(2) + "mb";
-}
+  const weatherObject = await weatherJSON.then(function (response) {
+    return response;
+  });
+  weatherHead.textContent = "Currently viewing data for: " + weatherObject.name;
+  weatherType.innerHTML =
+    weatherObject.weather[0].main +
+    `
+                            <small class="text-muted" id="desc">` +
+    weatherObject.weather[0].description +
+    `</small>`;
+  tempVal.textContent =
+    "Temperature: " + toCelcStr(weatherObject.main.temp) + "°C";
+  tempApp.textContent =
+    "Apparent Temperature: " + toCelcStr(weatherObject.main.feels_like) + "°C";
+  tempMax.textContent =
+    "Maximum Temperature: " + toCelcStr(weatherObject.main.temp_max) + "°C";
+  tempMin.textContent =
+    "Minimum Temperature: " + toCelcStr(weatherObject.main.temp_min) + "°C";
+  humVal.textContent =
+    "Humidity: " + weatherObject.main.humidity.toFixed(2) + "%";
+  presVal.textContent =
+    "Pressure: " + weatherObject.main.pressure.toFixed(2) + "mb";
+};
 
-formElem.addEventListener("submit", function (event) {event.preventDefault(); displayData(fetchData(createURL(searchBar, APIKey)));});
+formElem.addEventListener("submit", function (event) {
+  event.preventDefault();
+  displayData(fetchData(createURL(searchBar, APIKey)));
+});
